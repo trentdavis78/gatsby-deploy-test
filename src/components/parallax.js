1c26e840bc15d64b638775d5a0d89d6ee20d80fa
Trent Davis <trentdavisinc@gmail.com>
2019-05-29 03:18:39 -0500

    Parallax

--- src/components/parallax.js
+++ src/components/parallax.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
-
 export class parallax extends Component {
+  constructor(props) {
+    super(props);
 
+    this.M = window.M; /*"window.M" will make sure that you have access to the M included in the earlier materialize cdn script tag, so that you don't get errors like "M is undefined" */
+  }
+  componentDidMount() {
+    var elems1 = document.querySelectorAll(".parallax");
+    var instances1 = this.M.Parallax.init(elems1);
+  }
   render() {
     return (
         <div className="parallax-container">
